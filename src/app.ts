19acf6d92b06e327273280143f32e1f10468c3eb
Freewind <nowindlee@gmail.com>
2023-02-21 22:45:13 +0800

    use typing

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-Ext.onReady?.( () =>{
-    Ext.create?.('Ext.Panel', {
+Ext.onReady?.(() => {
+    new Ext.Panel(<Ext.panel.Panel.Cfg>{
         renderTo: 'main',
         height: 100,
         width: 200,
         title: 'Hello world',
         html: 'Hello Ext!'
-    });
+    })
 });
