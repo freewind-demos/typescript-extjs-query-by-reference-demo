4712061fb03698eef78da696836c76f1f701e844
Freewind <nowindlee@gmail.com>
2023-03-01 15:29:45 +0800

    typescript-extjs-show-array-data-in-panel-demo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-TypeScript ExtJS Demo
+TypeScript ExtJS Show Array Data in Panel Demo
 ====================
 
-'extjs-typings-6.2.0' 来自：https://github.com/ExtTS/definitions/tree/master/6.2.0-classic-all-packages-no-jsdocs
+可以使用getter来从`this.`中取数据。
 
 ```
 npm install
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 Ext.onReady(() => {
-    new Ext.Panel({
+    new Ext.panel.Panel({
         renderTo: 'main',
         height: 100,
         width: 200,
-        title: 'Hello world',
-        html: 'Hello Ext!'
+        title: 'Hello',
+        viewModel: new Ext.app.ViewModel({
+            data: {
+                colors: ['red', 'green', 'blue', 'white', 'black']
+            }
+        }),
+        get items() {
+            const self = this as Ext.panel.Panel.Cfg;
+            return self.viewModel?.data?.colors.map(color => new Ext.button.Button({ text: color }));
+        }
     })
 });
